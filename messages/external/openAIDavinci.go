6cb68caf8d8fee90719c2f157f89ca47b00d6c55
David <Nexfortisme@gmail.com>
2023-03-14 09:52:12 -0400

    Chat GPT Model WIP

--- messages/external/openAIDavinci.go
+++ messages/external/openAIDavinci.go
@@ -12,25 +12,32 @@ import (
 func GetOpenAIResponse(prompt string, openAIKey string) string {
 	client := &http.Client{}
 
-	dataTemplate := `{
-		"model": "text-davinci-003",
-		"prompt": "%s",
-		"temperature": 0.7,
-		"max_tokens": 256,
-		"top_p": 1,
-		"frequency_penalty": 0,
-		"presence_penalty": 0
-	  }`
+	// dataTemplate := `{
+	// 	"model": "gpt-3.5-turbo",
+	// 	"prompt": "%s",
+	// 	"temperature": 0.7,
+	// 	"max_tokens": 256,
+	// 	"top_p": 1,
+	// 	"frequency_penalty": 0,
+	// 	"presence_penalty": 0
+	//   }`
+	dataTemplate := `
+		"model": "gpt-3.5-turbo",
+		"messages": [{"role": "user", "content": "%s"}]
+	`
 	data := fmt.Sprintf(dataTemplate, prompt)
 
-	req, err := http.NewRequest(http.MethodPost, "https://api.openai.com/v1/completions", strings.NewReader(data))
+	fmt.Println(data)
+
+	req, err := http.NewRequest(http.MethodPost, "https://api.openai.com/v1/chat/completions", strings.NewReader(data))
 	if err != nil {
 		logging.LogError("Error creating POST request")
-		// log.Fatalf("Error creating POST request")
 	}
 
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Bearer "+openAIKey)
+
+	fmt.Println(req.Body)
 
 	resp, _ := client.Do(req)
 
@@ -47,11 +54,13 @@ func GetOpenAIResponse(prompt string, openAIKey string) string {
 		return ""
 	}
 
+	fmt.Println(string(buf))
+
 	// It's possible that OpenAI returns no response, so
 	// fallback to a default one
 	if len(rspOAI.Choices) == 0 {
 		return "I'm sorry, I don't understand?"
 	} else {
-		return rspOAI.Choices[0].Text
+		return rspOAI.Choices[0].Message.Content
 	}
 }
--- messages/external/openAIStructs.go
+++ messages/external/openAIStructs.go
@@ -2,9 +2,12 @@ package external
 
 // The outer structure of the response from OpenAI
 type OpenAIResponse struct {
-	ID      string         `json:"id"`
-	Object  string         `json:"object"`
-	Model   string         `json:"model"`
+	ID     string `json:"id"`
+	Object string `json:"object"`
+	Created string `json:"created"`
+	Model string `json:"model"`
+	Usage OpenAIUsage `json:"usage"`
+	// Model   string         `json:"model"`
 	Choices []OpenAIChoice `json:"choices"`
 }
 
@@ -12,9 +15,23 @@ type OpenAIResponse struct {
 // contains zero or more completions based on the provided
 // prompt
 type OpenAIChoice struct {
-	Text   string `json:"text"`
-	Index  int    `json:"index"`
-	Reason string `json:"finish_reason"`
+	Index         string        `json:"index"`
+	Message       OpenAIMessage `json:"message"`
+	Finish_Reason string        `json:"finish_reason"`
+	// Text   string `json:"text"`
+	// Index  int    `json:"index"`
+	// Reason string `json:"finish_reason"`
+}
+
+type OpenAIUsage struct {
+	PromtTokens int `json:"prompt_tokens"`
+	CompletionTokens int `json:"completion_tokens"`
+	TotalTokens int `json:"total_tokens"`
+}
+
+type OpenAIMessage struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
 }
 
 type DalleResponse struct {
