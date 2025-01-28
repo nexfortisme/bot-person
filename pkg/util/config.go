f13219794137258ecdb6ab88b00d27e1601329ce
David <nexfortisme@gmail.com>
2025-01-28 08:42:26 -0500

    Updated config and example .env

--- pkg/util/config.go
+++ pkg/util/config.go
@@ -9,12 +9,6 @@ import (
 )
 
 var (
-	db_host      string
-	db_user      string
-	db_password  string
-	db_namespace string
-	db_name      string
-
 	open_ai_api_key     string
 	discord_api_key     string
 	dev_discord_api_key string
@@ -36,38 +30,12 @@ func ReadEnv() {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	db_host = os.Getenv("DB_HOST")
-	db_user = os.Getenv("DB_USER")
-	db_password = os.Getenv("DB_PASSWORD")
-	db_namespace = os.Getenv("DB_NAMESPACE")
-	db_name = os.Getenv("DB_NAME")
-
 	open_ai_api_key = os.Getenv("OPEN_AI_API_KEY")
 	discord_api_key = os.Getenv("DISCORD_API_KEY")
 	dev_discord_api_key = os.Getenv("DEV_DISCORD_API_KEY")
 	finn_hub_api_key = os.Getenv("FINNHUB_API_KEY")
 	elevenlabs_api_key = os.Getenv("ELEVEN_LABS_API_KEY")
 	perplexity_api_key = os.Getenv("PERPLEXITY_API_KEY")
-}
-
-func GetDBHost() string {
-	return db_host
-}
-
-func GetDBUser() string {
-	return db_user
-}
-
-func GetDBPassword() string {
-	return db_password
-}
-
-func GetDBNamespace() string {
-	return db_namespace
-}
-
-func GetDBName() string {
-	return db_name
 }
 
 func GetOpenAIKey() string {
